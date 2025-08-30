1b0fb79a0065dce6b3c28be617e5749a6df6145a
Rakibul Islam <rakibulislam10.net@gmail.com>
2025-08-30 11:59:40 +0600

    Create ml.sh

---
+++ ml.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+DESCRIPTION=$2
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "❌ Please provide a project name."
+  echo "Usage: ./generate_pubspec.sh <project_name> \"<description>\""
+  exit 1
+fi
+
+if [ -z "$DESCRIPTION" ]; then
+  DESCRIPTION="A new Flutter project."
+fi
+
+cat <<EOF > pubspec.yaml
+name: $PROJECT_NAME
+description: "$DESCRIPTION"
+publish_to: 'none'
+
+version: 1.0.0+1
+
+environment:
+  sdk: ^3.9.0
+
+dependencies:
+  flutter:
+    sdk: flutter
+
+  cupertino_icons: ^1.0.8
+  shimmer:
+  url_launcher: ^6.3.1
+  pin_code_fields:
+  loading_animation_widget:
+  http_parser: ^4.0.2
+  mime: ^1.0.4
+  flutter_screenutil: ^5.9.3
+  flutter_animate:
+  get:
+  google_sign_in: ^6.3.0
+  get_storage:
+  connectivity_plus:
+  flutter_svg:
+  intl:
+  cached_network_image:
+  http:
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+  flutter_lints: ^5.0.0
+
+flutter:
+
+  uses-material-design: true
+
+  assets:
+    - assets/icons/
+    - assets/logo/
+    
+# fonts:
+#   - family: Schyler
+#     fonts:
+#       - asset: fonts/Schyler-Regular.ttf
+#       - asset: fonts/Schyler-Italic.ttf
+#         style: italic
+EOF
+
+echo "✅ pubspec.yaml generated for project '$PROJECT_NAME'"
