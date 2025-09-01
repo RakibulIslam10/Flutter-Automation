b320a9da6e8a834a8c69adb7f87b957af72ad358
Rakibul Islam <rakibulislam10.net@gmail.com>
2025-09-01 11:00:25 +0600

    Update rakib.sh

--- rakib.sh
+++ rakib.sh
@@ -5,38 +5,60 @@ case "$1" in
 "generate-views")
     # shellcheck disable=SC2162
     read -p "📥 Enter View Names (space-separated): " viewNames
-    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/refs/heads/main/view.sh | bash -s $viewNames
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/vv.sh | bash -s $viewNames
     ;;
 
 "generate-widget")
     # shellcheck disable=SC2162
     read -p "Enter View Name: " viewName
     # shellcheck disable=SC2162
-    read -p "📥 Enter View Names (space-separated): " widgetNames
+    read -p "📥 Enter Widget Names (space-separated): " widgetNames
     IFS=' ' read -r -a widgetArray <<< "$widgetNames"
-    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/main/widget | bash -s "$viewName" "${widgetArray[@]}"
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/widt.sh | bash -s "$viewName" "${widgetArray[@]}"
     ;;
 
 "create-structure")
     echo "🛠️ Creating Flutter project folder structure..."
-    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/refs/heads/main/stucture.sh | bash
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/cs.sh | bash
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/cu.sh | bash
+#    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/py.sh | bash
     ;;
+
 "generate-api-method")
     echo "🛠️ Creating Api Method..."
-    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/refs/heads/main/api_method.sh | bash
-    ;;
-
-"get-dependencies")
-    echo "🔽 Downloading & applying dependencies..."
-    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/refs/heads/main/get_dependencies.sh | bash
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/am.sh | bash
     ;;
 
 
-*)
-    echo "❌ Unknown command: $1"
-    echo "👉 Available commands:"
-    echo "   generate-views"
-    echo "   generate-widget"
-    echo "   create-structure"
+"generate-clean-yaml")
+    # 📥 Ask user for project name and description
+    # shellcheck disable=SC2162
+    read -p "📥 Enter Project Name: " projectName
+    # shellcheck disable=SC2162
+    read -p "📥 Enter Project Description: " projectDescription
+
+    # 🔗 Call remote script with projectName and description as arguments
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/ml.sh | bash -s "$projectName" "$projectDescription"
     ;;
+
+#"get-dependencies")
+#    echo "🔽 Downloading & applying dependencies..."
+#    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/py.sh | bash
+#    ;;
+
+"setup-firebase")
+    echo "📥 Downloading Firebase setup script..."
+    curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/frbs.sh -o frbs.sh
+    chmod +x frbs.sh
+    echo "🚀 Running Firebase setup..."
+    ./frbs.sh
+    ;;
+  
+  
+  "create-shaKey")
+      echo "🛠️ Creating SHA Key..."
+      curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Flutter-Automation/refs/heads/main/sa.sh | bash
+      ;;
+
+
 esac
