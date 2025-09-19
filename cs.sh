76c8c792576afdab4cbb94ecafc23f82f4f5a362
Rakibul Islam <rakibulislam10.net@gmail.com>
2025-09-19 11:50:06 +0600

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -89,7 +89,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(411, 915),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) => GetMaterialApp(
